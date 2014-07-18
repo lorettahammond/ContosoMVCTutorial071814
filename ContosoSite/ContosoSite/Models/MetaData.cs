d34e8d2f62ce616fd72eeaca54e1ed1ab8dbb40f
lorettahammond <lhammo09@baker.edu>
2014-07-18 11:10:26 -0400

    ContosoDatabase
    Added annotation of string length in Models>Student.cs; added annotation
    of range in Models>Enrollment.cs; verified validation  rules in Website
    for Student names & grade; added Metadata.cs and PartialClass.cs to
    Models;  folder; updated, refreshed, & saved .edmx file in VS; verified
    that validation rules still apply within Webpages

---
+++ ContosoSite/ContosoSite/Models/MetaData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ContosoSite.Models
+{
+    public class StudentMetadata
+    {
+        [StringLength(50)]
+        public string LastName;
+        [StringLength(50)]
+        public string FirstName;
+        [StringLength(50)]
+        public string MiddleName;
+    }
+    public class EnrollmentMetadata { [Range(0, 4)] public Nullable<decimal> Grade; }
+}
